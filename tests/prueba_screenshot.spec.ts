6393a1034dba06004b2d4e984fac9b642a7b752f
Alejandro Palacios <srjalean@gmail.com>
2024-04-20 23:54:52 +0200

    Reorganización de archivos y pruebas con 'screenshots'

---
+++ tests/prueba_screenshot.spec.ts
@@ -0,0 +1,9 @@
+import {test, expect} from '@playwright/test';
+
+test('purchase an item', async ({page}) => {
+
+    await page.goto('https://www.saucedemo.com/')
+
+    await page.screenshot({path: 'screenshot/login.png' })
+
+})
