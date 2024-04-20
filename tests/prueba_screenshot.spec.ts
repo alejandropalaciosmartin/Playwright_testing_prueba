f71a7611311e06fef79dd53642486d3152315517
Alejandro Palacios <srjalean@gmail.com>
2024-04-21 00:06:45 +0200

    Documentación de screenshots finalizada

--- tests/prueba_screenshot.spec.ts
+++ tests/prueba_screenshot.spec.ts
@@ -4,6 +4,6 @@ test('purchase an item', async ({page}) => {
 
     await page.goto('https://www.saucedemo.com/')
 
-    await page.screenshot({path: 'screenshot/login.png' })
+    await page.screenshot({path: 'screenshot/login.png', fullPage: true })
 
 })
